aa72df23123597a495f212338cae618b40a42964
Nurmuhass <nurmuhass@gmail.com>
2024-06-13 14:20:14 +0100

    changes

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import ColorList from '../components/ColorList'
+
+const index = () => {
+  return (
+    <View>
+       <ColorList color="#0891b2" />
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
